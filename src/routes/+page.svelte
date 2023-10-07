99874618cc1ff74e8c64928c9790bc07190985bc
Simone Salerno <contact@simonesalerno.it>
2023-10-07 15:27:21 +0200

    New contact version

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,10 @@
 <Navbar />
 
 <div
-	class="flex flex-col xl:flex-row gap-10 2xl:gap-20 border-b border-white border-opacity-5 px-4 sm:px-16 lg:px-32 py-28 sm:py-40"
+	class="flex flex-col xl:flex-row gap-10 2xl:gap-20 border-b border-white border-opacity-5 px-4 sm:px-16 lg:px-32 2xl:px-44 py-28 sm:py-40"
 >
 	<div
-		class="flex xl:w-[65%] 2xl:w-[55%] flex-col tracking-tighter leading-[0.7] justify-center gap-y-7 sm:gap-y-10"
+		class="flex xl:w-[65%] 2xl:w-[60%] flex-col tracking-tighter leading-[0.7] justify-center gap-y-7 sm:gap-y-10"
 	>
 		<div class="text-7xl 2xl:text-[5.5rem]">
 			<p>Hello, I'm Simone</p>
@@ -31,7 +31,7 @@
 	</div>
 
 	<div
-		class="flex flex-col xl:w-[35%] 2xl:w-[45%] text-2xl xl:text-xl 2xl:text-2xl opacity-80 gap-y-4 sm:gap-y-6 justify-center"
+		class="flex flex-col xl:w-[35%] 2xl:w-[40%] text-2xl xl:text-xl 2xl:text-2xl opacity-80 gap-y-4 sm:gap-y-6 justify-center"
 	>
 		<p>Full-Stack developer who loves diving into innovative projects and solving problems.</p>
 		<p>
@@ -98,85 +98,85 @@
 
 <div
 	id="contact"
-	class="flex flex-col items-center xl:flex-row gap-16 sm:gap-20 2xl:gap-28 border-t border-white border-opacity-5 xl:px-32 2xl:px-44 pt-24 pb-28 sm:pt-32 sm:pb-36"
+	class="flex flex-col items-center border-t border-white border-opacity-5 px-4 sm:px-16 lg:px-32 2xl:px-44 pt-24 pb-28 sm:pt-32 sm:pb-36"
 >
-	<div
-		class="flex xl:w-[60%] flex-col tracking-tight justify-between gap-y-16 sm:gap-y-24 px-4 sm:px-16 lg:px-32 xl:px-0"
-	>
-		<div class="flex flex-col gap-y-7">
-			<h3 class="text-5xl sm:text-6xl">Available for select freelance opportunities</h3>
-			<p class="text-2xl">
+	<div class="flex w-full flex-col tracking-tight justify-between gap-y-16 sm:gap-y-24">
+		<div class="flex flex-col gap-y-10">
+			<h3 class="text-5xl md:text-6xl">Available for select freelance opportunities</h3>
+			<p class="text-2xl md:text-3xl">
 				Have an engaging project in mind? Feel free to reach out to me by email or instant message.
 			</p>
 		</div>
 
-		<div class="flex flex-col text-xl gap-y-4">
+		<div class="flex flex-col text-2xl gap-y-4">
 			<div>
-				<a class="text-2xl" href="mailto:contact@simonesalerno.it">contact@simonesalerno.it</a>
+				<a
+					class="group transition-all duration-300 ease-in-out"
+					href="mailto:contact@simonesalerno.it"
+					target="_blank"
+				>
+					<span
+						class="text-3xl md:text-4xl bg-left-bottom bg-gradient-to-r from-gray-300 to-gray-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+					>
+						contact@simonesalerno.it
+					</span>
+				</a>
 			</div>
 			<div class="flex flex-col gap-y-2">
 				<div>
-					<a href="https://www.linkedin.com/in/simone-salerno/" target="_blank">LinkedIn</a>
+					<a
+						class="group transition-all duration-300 ease-in-out"
+						href="https://www.linkedin.com/in/simone-salerno/"
+						target="_blank"
+					>
+						<span
+							class="bg-left-bottom bg-gradient-to-r from-gray-300 to-gray-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+						>
+							LinkedIn
+						</span>
+					</a>
 				</div>
-				<div><a href="https://github.com/novakoder/" target="_blank">GitHub</a></div>
-				<div><a href="https://www.instagram.com/novakoder/" target="_blank">Instagram</a></div>
-				<div><a href="https://twitter.com/novakoder/" target="_blank">X</a></div>
+				<div>
+					<a
+						class="group transition-all duration-300 ease-in-out"
+						href="https://github.com/novakoder/"
+						target="_blank"
+					>
+						<span
+							class="bg-left-bottom bg-gradient-to-r from-gray-300 to-gray-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+						>
+							GitHub
+						</span>
+					</a>
+				</div>
+				<div>
+					<a
+						class="group transition-all duration-300 ease-in-out"
+						href="https://www.instagram.com/novakoder/"
+						target="_blank"
+					>
+						<span
+							class="bg-left-bottom bg-gradient-to-r from-gray-300 to-gray-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+						>
+							Instagram
+						</span>
+					</a>
+				</div>
+				<div>
+					<a
+						class="group transition-all duration-300 ease-in-out"
+						href="https://twitter.com/novakoder/"
+						target="_blank"
+					>
+						<span
+							class="bg-left-bottom bg-gradient-to-r from-gray-300 to-gray-300 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
+						>
+							X
+						</span>
+					</a>
+				</div>
 			</div>
 		</div>
-	</div>
-
-	<div class="flex flex-col w-full xl:w-[45%] gap-y-10 sm:gap-y-12 px-4 sm:px-16 lg:px-32 xl:px-0">
-		<h3 class="text-4xl xl:self-end">// contact module</h3>
-		<form class="flex flex-col gap-y-10" name="contact-form" method="POST" data-netlify="true">
-			<input type="hidden" name="form-name" value="contact-form" />
-
-			<div class="grid gap-x-6 gap-y-8 sm:grid-cols-2 opacity-80">
-				<div class="relative z-0">
-					<input
-						type="text"
-						name="name"
-						class="peer block w-full appearance-none border-0 border-b bg-transparent py-2.5 px-0 text-lg focus:border-blue-600 focus:outline-none focus:ring-0"
-						placeholder=" "
-					/>
-					<label
-						for="name"
-						class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-lg duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500"
-						>Your name</label
-					>
-				</div>
-				<div class="relative z-0">
-					<input
-						type="text"
-						name="email"
-						class="peer block w-full appearance-none border-0 border-b bg-transparent py-2.5 px-0 text-lg focus:border-blue-600 focus:outline-none focus:ring-0"
-						placeholder=" "
-					/>
-					<label
-						for="email"
-						class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-lg duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500"
-						>Your email</label
-					>
-				</div>
-				<div class="relative z-0 col-span-2">
-					<textarea
-						name="message"
-						rows="5"
-						class="peer block w-full appearance-none border-0 border-b bg-transparent py-2.5 px-0 text-lg focus:border-blue-600 focus:outline-none focus:ring-0"
-						placeholder=" "
-					/>
-					<label
-						for="message"
-						class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-lg duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500"
-						>Your message</label
-					>
-				</div>
-			</div>
-			<button
-				type="submit"
-				class="xl:self-end rounded-full bg-white bg-opacity-[0.02] border border-white border-opacity-10 w-[60%] sm:w-[45%] pt-[0.63rem] pb-3 text-white text-xl shadow-md hover:scale-105 transition-transform duration-300"
-				>Send Message</button
-			>
-		</form>
 	</div>
 </div>
 
